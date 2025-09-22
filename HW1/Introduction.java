6d9ea0555029c2c6decc8698473485cb485b380e
Hieu <24021473@vnu.edu.vn>
2025-09-22 10:03:22 +0700

    lam bai tap phan B

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -13,19 +13,19 @@ public class Introduction {
         System.out.println("Take one down, pass it around,");
         System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
         System.out.println("Take one down, pass it around,");
-        System.out.println("7 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("7 bottles of beer on the wall, 7 bottles of beer.");
         System.out.println("Take one down, pass it around,");
-        System.out.println("6 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("6 bottles of beer on the wall, 6 bottles of beer.");
         System.out.println("Take one down, pass it around,");
-        System.out.println("5 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("5 bottles of beer on the wall, 5 bottles of beer.");
         System.out.println("Take one down, pass it around,");
-        System.out.println("4 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("4 bottles of beer on the wall, 4 bottles of beer.");
         System.out.println("Take one down, pass it around,");
-        System.out.println("3 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("3 bottles of beer on the wall, 3 bottles of beer.");
         System.out.println("Take one down, pass it around,");
-        System.out.println("2 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("2 bottles of beer on the wall, 2 bottles of beer.");
         System.out.println("Take one down, pass it around,");
-        System.out.println("1 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("1 bottles of beer on the wall, 1 bottles of beer.");
         System.out.println("Take one down, pass it around,");
         System.out.println("No more bottles of beer on the wall");
     }
