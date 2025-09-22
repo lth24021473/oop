724e55b5bcd7bfde2c89dc29f186005e0206f5a2
Hieu <24021473@vnu.edu.vn>
2025-09-22 10:01:36 +0700

    lam bai tap phan B

---
+++ HW1/Introduction.java
@@ -0,0 +1,32 @@
+package HW1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Lê Trung Hiếu\t" +
+                "24021473\t" +
+                "CS5\t" +
+                "@lth24021473\t" +
+                "lthieu15112000006@gmail.com");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("7 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("6 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("5 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("4 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("3 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("2 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("1 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall");
+    }
+}
